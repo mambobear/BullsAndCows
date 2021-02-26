18d43a9f28280f1b212619f2c971002a1651eb2b
Andrei Zherebtsov <azherebtsov@gmail.com>
2021-02-25 17:07:11 -0800

    Finish project

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -4,9 +4,11 @@ import java.util.*;
 
 public class Main {
 
-    public static String secretCode;
+    private static String secretCode;
 
     public static void main(String[] args) {
+
+        System.out.println(Main.class.getName());
 
         // stage 1
         // printGameLog();
@@ -63,19 +65,128 @@ public class Main {
         playBullsAndCows();
 
         // stage 5. Improve the code generator
+
+        // stage 6.
+        // Description
+        /*
+        * Some players need a challenge, so let's make the secret code in the game harder to guess.
+        *  Add support for more than 10 symbols by adding letters. Now, the secret code can contain
+        * numbers 0-9 and lowercase Latin characters a-z. The new maximum length for the code is 36.
+        * Note that the length of the secret word may not match the size of possible characters in
+        * the secret code, so you should make two separate inputs for the secret code length and for
+        * the size of possible characters.  Also, since a secret code is not a number anymore, the
+        * symbol 0 should be allowed as the first character in a secret code.
+        *
+        */
+
+        // Objectives
+        /*
+        * In this step, your program should:
+        *   1. Ask for the length of the secret code.
+        *   2. Ask for the range of possible characters in the secret code.
+        *   3. Generate a secret code using numbers and characters. This time, you should also print the secret
+        *      code using * characters and print which characters were used to generate the secret code.
+        */
+
+        // Examples
+        /*
+        *
+        * Input the length of the secret code:
+        * > 4
+        * Input the number of possible symbols in the code:
+        * > 16
+        * The secret is prepared: **** (0-9, a-f).
+        * Okay, let's start a game!
+        * Turn 1:
+        * > 1a34
+        * Grade: 1 bull and 1 cow
+        * Turn 2:
+        * > b354
+        * Grade: 2 bulls and 1 cow
+        * Turn 3:
+        * > 93b4
+        * Grade: 4 bulls
+        * Congratulations! You guessed the secret code.
+        *
+        */
+
+        // stage 7.
+        // Description
+        /*
+        *
+        * There are a lot of error possibilities. What if someone enters an answer of the wrong length?
+        *  Or the number of possible symbols is less than the length of the code? What if the answer
+        * contains invalid symbols? The game may crash before the secret number was guessed!
+        *
+        * Let's handle errors like this. At this point, you can implement this without the try catch construction.
+        * Use the following rule of thumb: if you can avoid the exception-based logic, avoid it! If you use exceptions
+        * in normal situations, how would you deal with unusual (exceptional) situations? Now it may not seem that
+        * important, but when you need to find errors in more complex programs, this makes a difference.
+        *
+        * */
+        // Objectives
+        /*
+        * In this stage, your program should:
+        *   1. Handle incorrect input.
+        *   2. Print an error message that contains the word error. After that, don't ask for the numbers again,
+        *      just finish the program.
+        * */
+        // Examples
+        /*
+        * Example 1
+        * Input the length of the secret code:
+        * > 6
+        * Input the number of possible symbols in the code:
+        * > 5
+        * Error: it's not possible to generate a code with a length of 6 with 5 unique symbols.
+        *
+        * Example 2
+        * Input the length of the secret code:
+        * > abc 0 -7
+        * Error: "abc 0 -7" isn't a valid number.
+        *
+        * Example 3
+        * Input the length of the secret code:
+        * > 6
+        * Input the number of possible symbols in the code:
+        * > 37
+        * Error: maximum number of possible symbols in the code is 36 (0-9, a-z).
+        *
+        * Example 4
+        * Input the length of the secret code:
+        * > 4
+        * Input the number of possible symbols in the code:
+        * > 12
+        * The secret is prepared: **** (0-9, a-b).
+        * Okay, let's start a game!
+        * Turn 1:
+        * > a234
+        * Grade: 1 bull and 1 cow
+        * Turn 2:
+        * > 73b4
+        * Grade: 2 bulls and 1 cow
+        * Turn 3:
+        * > 9374
+        * Grage: 4 bulls
+        * Congratulations! You guessed the secret code.
+        *
+        * */
     }
 
     private static void playBullsAndCows() {
 
         Scanner scanner = new Scanner(System.in);
 
-        int bulls = 0;
+        int bulls;
         String guess;
         int[] grade;
         int turn = 1;
 
         secretCode = getSecretCode();
-        System.out.println("Okay, let's start a game!");
+        if ("".equals(secretCode)) {
+            return;
+        }
+        printGameInvitation();
 
         while (true) {
             System.out.printf("Turn %d:\n", turn++);
@@ -88,24 +199,79 @@ public class Main {
                 break;
             }
         }
+    }
 
+    private static void printGameInvitation() {
+        System.out.println("Okay, let's start a game!");
     }
 
     private static String getSecretCode() {
         Scanner scanner = new Scanner(System.in);
-        int numDigits;
+        int numSymbols;
+        int length;
         String code;
         while (true) {
-            System.out.println("Please, enter the secret code's length:");
-            numDigits = scanner.nextInt();
-            if (numDigits > 10) {
-                System.out.println("Error: can't generate a secret number with a " +
-                        "length of 11 because there aren't enough unique digits.");
+            System.out.println("Input the length of the secret code:");
+            String str = scanner.nextLine();
+            try {
+                length = Integer.parseInt(str);
+            } catch (NumberFormatException  e) {
+                System.out.printf("Error: \"%s\" isn't a valid number.\n", str);
+                return "";
+            }
+            if (length > 36) {
+                System.out.println("Error: can't generate a secret code of " +
+                        "length greater than 36 because there aren't enough unique" +
+                        " symbols.");
+                return "";
+            }
+            if (length <= 0) {
+                System.out.println("Error: length cannot be less than 1");
+                return "";
+            }
+
+            System.out.println("Input the number of possible symbols in the code:");
+            str = scanner.nextLine();
+            try {
+                numSymbols = Integer.parseInt(str);
+            } catch (NumberFormatException  e) {
+                System.out.printf("Error: \"%s\" isn't a valid number.\n", str);
+                return "";
+            }
+            if (numSymbols > 36) {
+                System.out.println("Error: maximum number of possible symbols in the code is 36 (0-9, a-z).");
+                return "";
+            }
+            if (numSymbols < 10) {
+                System.out.println("Error: number of symbols cannot be less than 10");
+                return "";
+            }
+
+            if (length > numSymbols) {
+                System.out.printf("Error: it's not possible to generate a code with a length of %d with %d " +
+                        "unique symbols.\n", length, numSymbols);
+                return "";
+            }
+
+            code = generateCode(length, numSymbols);
+            break;
+        }
+        StringBuilder strb = new StringBuilder("The secret is prepared: ");
+        strb.append("*".repeat(Math.max(0, length)));
+        strb.append(" (0-9");
+        if (numSymbols == 10) {
+            strb.append(')');
+        } else {
+            strb.append(", a");
+            if(numSymbols - 10 == 1) {
+                strb.append(')');
             } else {
-                code = generateCode(numDigits);
-                break;
+                strb.append('-');
+                strb.append((char) (numSymbols - 11 + 'a'));
+                strb.append(").");
             }
         }
+        System.out.println(strb.toString());
         return code;
     }
 
@@ -117,23 +283,27 @@ public class Main {
             System.out.println("Error: can't generate a secret number with a " +
                     "length of 11 because there aren't enough unique digits.");
         } else {
-            System.out.println(generateCode(numDigits));
+            // TODO:
+            System.out.println(generateCode(numDigits, 0));
         }
     }
 
-    private static String generateCode(int numDigits) {
-        ArrayList<Integer> digits =  new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+    private static String generateCode(int length, int numSymbols) {
+
+
+        ArrayList<Character> symbols = new ArrayList<>();
+        String str = "0123456789abcdefghijklmnopqrstuvwyyz".substring(0, numSymbols);
+        for (char c : str.toCharArray()) {
+            symbols.add(c);
+        }
+
         StringBuilder strb = new StringBuilder();
         Random random = new Random(System.nanoTime());
 
-        int idx = random.nextInt(digits.size() - 1) + 1;
-        strb.append(digits.get(idx));
-        digits.remove(idx);
-
-        for (int i = 1; i < numDigits; i++) {
-            idx = random.nextInt(digits.size());
-            strb.append(digits.get(idx));
-            digits.remove(idx);
+        for (int i = 0; i < length; i++) {
+            int idx = random.nextInt(symbols.size());
+            strb.append(symbols.get(idx));
+            symbols.remove(idx);
         }
         return strb.toString();
     }
@@ -156,7 +326,7 @@ public class Main {
         } else if (cows > 0) {
             System.out.printf("Grade : %d %s\n", cows, cowsStr);
         } else {
-            System.out.printf("Grade : None\n");
+            System.out.print("Grade : None\n");
         }
     }
 
@@ -174,7 +344,6 @@ public class Main {
     }
 
     private static void printGameLog() {
-        int secretCode = 9876;
         System.out.println("The secret code is prepared: ****.");
 
         System.out.println("Turn 1. Answer:\n" +
